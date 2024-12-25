a43eb1158dde3e75b824d21a56735f994a28bf66
peyush <piyushnawal19.com>
2024-12-25 11:37:45 +0530

    "unwanted import fix "

--- src/pages/FindDoctors.tsx
+++ src/pages/FindDoctors.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import  { useEffect, useRef, useState } from 'react'
 import { MdLocationPin } from "react-icons/md";
 import { IoIosArrowDown } from "react-icons/io";
 import { FaArrowRightLong } from "react-icons/fa6";
